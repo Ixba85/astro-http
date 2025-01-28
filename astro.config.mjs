ad80e2094db42d0c7b1647a0dad1794eb0b43fe4
Ixba85 <jordicomasa@gmail.com>
2025-01-28 10:36:03 -0600

    add static render

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,5 +11,6 @@ import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
+  output: "static",
   adapter: cloudflare(),
 });
